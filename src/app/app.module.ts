985be8da7ea32460fe7f598e8b0d4ec782cdc607
nodemonchik <nodemonchik@yandex.ru>
2020-01-13 19:03:08 +0300

    generate task components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskFormComponent,
+    TaskListComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
